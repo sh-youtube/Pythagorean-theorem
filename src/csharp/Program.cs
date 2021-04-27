c2111b73334d3d487290e77087484d9cabb4fd6c
m_shabne <a.shabane@gmail.com>
2021-04-27 15:59:28 +0430

    c#

---
+++ src/csharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            initier:
+            Console.Write("Enter Side A[ctrl+c to quit]: ");
+            double a = int.Parse(Console.ReadLine());
+            Console.Write("Enter Side B[ctrl+c to quit]: ");
+            double b = int.Parse(Console.ReadLine());
+            double c = Math.Sqrt((Math.Pow(a, 2) + Math.Pow(b, 2)));
+            Console.WriteLine("C side is: "+c+"\n");
+            goto initier;
+        }
+    }
+}
